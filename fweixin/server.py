e9f01c48215db5e58549a6a8d77ab071e825aa7b
root <aliyun@wy.com>
2018-08-09 19:03:28 +0800

    save

--- fweixin/server.py
+++ fweixin/server.py
@@ -146,4 +146,4 @@ if __name__ == '__main__':
     login_thread.start()
     sync = tools.sync_task()
     next(sync)
-    app.run(host='127.0.0.1', port=8000, debug=False)
+    app.run(host='127.0.0.1', port=8001, debug=False)
